797535970044b9d533158e3ea2c5c2d2ce977849
bhaskar345 <bhaskarchauhan222@gmail.com>
2026-03-23 23:35:55 +0530

    updated model

--- Backend/app/apps.py
+++ Backend/app/apps.py
@@ -1,14 +1,6 @@
-import os
-from .startup import ensure_model
 from django.apps import AppConfig
 
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
-
-    def ready(self):
-        if os.environ.get("RUN_MAIN") != "true":
-            return
-
-        ensure_model()
